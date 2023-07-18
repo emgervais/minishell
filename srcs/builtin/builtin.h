6d7bdc945c26102e3467b9d1f2d63cb19ad5a753
emgervais <129791447+emgervais@users.noreply.github.com>
2023-07-18 19:41:38 -0400

    Add files via upload

---
+++ srcs/builtin/builtin.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egervais <egervais@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 15:22:47 by egervais          #+#    #+#             */
+/*   Updated: 2023/07/18 19:20:43 by egervais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_H
+# define BUILTIN_H
+
+# include "minishell.h"
+
+char *add_one_char(char *s1, char c, int malloced);
+
+#endif
