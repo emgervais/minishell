ff1b81a8c8f5e0a3034328e4d2b607f115511734
Emile Le Sage <emile.lesage8@gmail.com>
2023-09-01 14:00:43 -0400

    egervais me doit une pipe. Merci.

--- srcs/executor/fd_utils.c
+++ srcs/executor/fd_utils.c
@@ -6,7 +6,7 @@
 /*   By: ele-sage <ele-sage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 22:08:05 by ele-sage          #+#    #+#             */
-/*   Updated: 2023/08/31 20:23:53 by ele-sage         ###   ########.fr       */
+/*   Updated: 2023/09/01 13:58:18 by ele-sage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@ int	dup_fd(t_cmds *cmds)
 			return (error_fd(1, cmds));
 		close(cmds->fd.fd_in);
 	}
+	if (cmds->next && cmds->next->fd.fd_in != STDIN_FILENO)
+		close(cmds->next->fd.fd_in);
 	if (cmds->fd.fd_out != STDOUT_FILENO)
 	{
 		if (dup2(cmds->fd.fd_out, STDOUT_FILENO) == -1)
 			return (error_fd(1, cmds));
 		close(cmds->fd.fd_out);
 	}
+	if (cmds->prev && cmds->prev->fd.fd_out != STDOUT_FILENO)
+		close(cmds->prev->fd.fd_out);
 	return (SUCCESS);
 }
 
